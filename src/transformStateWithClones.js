6c3cc0d2b938ba009270d489725c25cc4bc10c22
notYourCode999 <virtualshot.one4@gmail.com>
2026-09-09 12:41:51 +0300

    Implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let currentState = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+    let newState = { ...currentState };
+
+    if (action.type === 'addProperties') {
+      newState = {
+        ...currentState,
+        ...action.extraData,
+      };
+    }
+
+    if (action.type === 'removeProperties') {
+      for (let j = 0; j < action.keysToRemove.length; j++) {
+        delete newState[action.keysToRemove[j]];
+      }
+    }
+
+    if (action.type === 'clear') {
+      const keys = Object.keys(newState);
+
+      for (let k = 0; k < keys.length; k++) {
+        delete newState[keys[k]];
+      }
+    }
+
+    history.push(newState);
+    currentState = newState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
